0b3d22bf2112de2d8c742434d7fc2e1cff2394ca
Xiaoying Zhao <zhaox39@uw.edu>
2020-03-06 14:11:03 -0800

    add data

---
+++ R/my_gapminder.R
@@ -0,0 +1,18 @@
+#' Gapminder data
+#'
+#' Excerpt of the \code{\link[datasets]{Gapminder}} dataset on life expectancy,
+#' GDP per capita, and population by country.
+#'
+#' @format a data frame with 150 observations and 3 variables:
+#' \describe{
+#' \item{country}{factor with 142 levels}
+#' \item{continent}{factor with 5 levels}
+#' \item{year}{ranges from 1952 to 2007 in increments of 5 years}
+#' \item{lifeExp}{life expectancy at birth, in years}
+#' \item{pop}{population}
+#' \item{gdpPercao}{GDP per capita (US$, inflation-adjusted)}
+#' }
+#'
+#' @source
+#' http://www.gapminder.org/data/
+"my_gapminder"
